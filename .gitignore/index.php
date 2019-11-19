9cc3b1c3cbb7173ea9eab37872a36910a89b1e72
neanderf <57958491+neanderf@users.noreply.github.com>
2019-11-19 15:45:41 -0300

    Delete index.php

--- .gitignore/index.php
+++
@@ -1,6 +0,0 @@
-<?php                    
-  echo "<h1>Openshift Workshop v1.0</h1> ";                    
-  $host= gethostname();
-  $ip = gethostbyname($host);                    
-  echo $ip;
-  ?>
