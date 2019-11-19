6598a18c0b2abe05942fb43e431ea34ea1b44ae7
neanderf <57958491+neanderf@users.noreply.github.com>
2019-11-19 16:03:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
   $host= gethostname();
   $ip = gethostbyname($host);                    
   echo $ip;
- 
+?>
+
+<?php
 echo "<h1>Openshift Workshop v2.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
