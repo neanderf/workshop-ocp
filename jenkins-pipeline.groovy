93e3d72c844f100354f595799027e57a51677c37
neanderf <57958491+neanderf@users.noreply.github.com>
2019-11-20 10:41:17 -0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,14 +1,14 @@
 node('maven') {
     stage('build') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'workshop-ocp', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'php01', showBuildLogs: 'true')
     }
     stage('verify') {
         echo 'dummy verification....'
     }
     stage('deploy') {
         input 'Manual Approval'
-        openshiftDeploy(deploymentConfig: 'workshop-ocp')
+        openshiftDeploy(deploymentConfig: 'php01')
     }
     stage('promoting to QA') {
        echo 'fake stage...'
