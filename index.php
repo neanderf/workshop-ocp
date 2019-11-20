8701089b6902a45b535e29534b1edb5aea5b4076
neanderf <57958491+neanderf@users.noreply.github.com>
2019-11-20 11:40:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php                    
-  echo "<h1>Openshift Workshop v2.0</h1> ";                    
+  echo "<h1>Openshift Workshop v1.0</h1> ";                    
   $host= gethostname();
   $ip = gethostbyname($host);                    
   echo $ip;
 ?>
 
-<?php
-echo "<h1>Openshift Workshop v2.0</h1> ";
-echo $_SERVER['SERVER_ADDR'];
-echo "<br><hr>";
-echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
-$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$result = $conn->query("SELECT nome FROM cidade");
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "<h3>" . $row["nome"] . "</h3>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
-?>
+#<?php
+#echo "<h1>Openshift Workshop v2.0</h1> ";
+#echo $_SERVER['SERVER_ADDR'];
+#echo "<br><hr>";
+#echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
+#$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
+#if ($conn->connect_error) {
+#    die("Connection failed: " . $conn->connect_error);
+#}
+#$result = $conn->query("SELECT nome FROM cidade");
+#if ($result->num_rows > 0) {
+#    while($row = $result->fetch_assoc()) {
+#        echo "<h3>" . $row["nome"] . "</h3>";
+#    }
+#} else {
+#    echo "0 results";
+#}
+#$conn->close();
+#?>
+#
